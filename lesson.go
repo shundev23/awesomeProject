fee618e1952e176d6d0d6eff3d1f420a7fc8e1e2
shundev23 <140191667+shundev23@users.noreply.github.com>
2024-12-08 00:30:40 +0900

    Update lesson.go
    Hello World

--- lesson.go
+++ lesson.go
@@ -1,1 +1,20 @@
 package main
+
+import "fmt"
+
+// mainより先に呼ばれる。
+// 初期設定で使用する
+/*func init() {
+	fmt.Println("Init")
+}
+*/
+
+// Goのエントリポイントはmainに限定されるため、mainで呼び出す必要あり
+func bazz() {
+	fmt.Println("Bazz")
+}
+
+func main() {
+	// bazz()
+	fmt.Println("Hello World", "test test")
+}
