ff8679ab7b6d763f65833a5d241d319fdab31230
shundev23 <140191667+shundev23@users.noreply.github.com>
2024-12-10 11:36:47 +0900

    Create type conversion.go

---
+++ 06_type conversion/type conversion.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	var x int = 1
+	xx := float64(x)
+	fmt.Printf("%T %v %f\n", xx, xx, xx)
+
+	var y float64 = 1.2
+	yy := int(y)
+	fmt.Printf("%T %v %d\n", yy, yy, yy)
+
+	var s string = "14"
+	i, _ := strconv.Atoi(s)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	fmt.Printf("%T %v\n", i, i)
+
+	h := "Hello World"
+	fmt.Println(string(h[0]))
+}
