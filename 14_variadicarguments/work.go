855957a1d128611e0cc1ea184eb894da173b30f2
shundev23 <140191667+shundev23@users.noreply.github.com>
2025-01-19 22:28:17 +0900

    Create work.go

---
+++ 14_variadicarguments/work.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func foo(params ...int) {
+	// ここに処理を記述
+	fmt.Println(len(params), params)
+	for _, param := range params {
+		fmt.Println(param)
+	}
+}
+
+func main() {
+	foo()
+	foo(10, 20)
+	foo(10, 20, 30)
+
+	s := []int{1, 2, 3}
+	fmt.Println(s)
+
+	foo(s...)
+}
